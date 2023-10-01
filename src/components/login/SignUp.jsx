1f214e191a6b050043aac882cadbc11c4869b26c
Borhan Uddin <68281712+wapborhan@users.noreply.github.com>
2023-10-01 15:31:25 +0600

    update

--- src/components/login/SignUp.jsx
+++ src/components/login/SignUp.jsx
@@ -39,7 +39,7 @@ const SignUp = () => {
         // ...
       })
       .catch((error) => {
-        const errorCode = error.code;
+        // const errorCode = error.code;
         const errorMessage = error.message;
 
         seMassege([
@@ -54,11 +54,11 @@ const SignUp = () => {
 
   return (
     <div>
-      <div className="signup-1 flex items-center relative h-screen">
+      <div className="signup-1 flex items-center relative h-screen outline-0">
         <div className="overlay absolute inset-0 z-0 bg-black opacity-75"></div>
         <div className="container px-4 mx-auto relative z-10">
           <div className="sm:w-10/12 md:w-8/12 lg:w-6/12 xl:w-5/12 mx-auto">
-            <div className="box bg-white p-6 md:px-12 md:pt-12 border-t-10 border-solid border-indigo-600 rounded-[2rem] border-t-[10px]">
+            <div className="box bg-white p-6 md:px-12 md:pt-6 border-t-10 border-solid border-indigo-600 rounded-[2rem] border-t-[10px]">
               <h2 className="text-3xl text-gray-800 text-center">
                 Create Your Account
               </h2>
@@ -82,7 +82,7 @@ const SignUp = () => {
                 </>
               )}
               <form onSubmit={handleRegister}>
-                <div className="signup-form mt-6 md:mt-12">
+                <div className="signup-form mt-6 md:mt-7">
                   <div className="border-2 border-solid rounded flex items-center mb-4">
                     <div className="w-10 h-10 flex justify-center items-center flex-shrink-0">
                       <span className="far fa-envelope text-gray-500">
@@ -152,7 +152,7 @@ const SignUp = () => {
                     </a>
                   </p>
 
-                  <div className="text-center mt-6 md:mt-12">
+                  <div className="text-center mt-4 md:mt-7">
                     <button className="bg-indigo-600 hover:bg-indigo-700 text-white text-xl py-2 px-4 md:px-6 rounded transition-colors duration-300">
                       Sign Up <span className="far fa-paper-plane ml-2"></span>
                     </button>
@@ -160,7 +160,7 @@ const SignUp = () => {
                 </div>
               </form>
 
-              <div className="border-t border-solid mt-6 md:mt-12 pt-4">
+              <div className="border-t border-solid mt-6 md:mt-7 pt-4">
                 <p className="text-gray-500 text-center">
                   Already have an account,{" "}
                   <NavLink
